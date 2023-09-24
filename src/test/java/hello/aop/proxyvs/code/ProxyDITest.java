3ec629e6ea2901f00f79d7574a505a25b44de2f7
highjune <highjune37@gmail.com>
2023-09-24 18:41:48 +0900

    last commit

--- src/test/java/hello/aop/proxyvs/code/ProxyDITest.java
+++ src/test/java/hello/aop/proxyvs/code/ProxyDITest.java
@@ -10,12 +10,12 @@ import org.springframework.context.annotation.Import;
 
 @Slf4j
 //@SpringBootTest(properties = {"spring.aop.proxy-target-class=false"}) // JDK 동적 프록시 사용. DI 예외 발생한다. 옵션을 넣지 않으면 스프링은 기본적으로 CGLIB 를 사용한다.
-@SpringBootTest(properties = {"spring.aop.proxy-target-class=true"}) // CGLIB 사용. DI 가능함
+//@SpringBootTest(properties = {"spring.aop.proxy-target-class=true"}) // CGLIB 사용. DI 가능함
+@SpringBootTest
 @Import(ProxyDIAspect.class)
 public class ProxyDITest {
 
     // 그래서 JDK 동적 프록시를 사용하면 항상 인터페이스에만 의존관계를 사용해야 한다. 구체클래스에 의존관계 사용 안된다.
-
     @Autowired
     MemberService memberService;
 
